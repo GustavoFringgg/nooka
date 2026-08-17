0c66f62bfa6f1e85ea1be8d31084c1f0e207cb6e
Derek <adamtsai0408@gmail.com>
2026-08-17 20:57:11 +0800

    feat(backend): 新增 CORS 設定,允許本機 Nuxt 開發環境（localhost:3000）跨域請求

--- backend/Nooka.Api/Program.cs
+++ backend/Nooka.Api/Program.cs
@@ -13,6 +13,14 @@ builder.Services.AddScoped<IWordRepository, EfWordRepository>();
 // 已 DI 處理，:「註冊以後有人要求 IWordRepository,就給他 EfWordRepository 的實例」
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddCors(options =>  // 註冊一定要在 builder 之前
+{
+    options.AddPolicy("NuxtDev", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
 
 var app = builder.Build();
 
@@ -24,8 +32,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors("NuxtDev");
+// UseCors 是中介軟體(middleware),ASP.NET Core 的中介軟體是照寫的順序一個一個執行,CORS 檢查一定要放在 UseAuthorization/MapControllers 之前,不然請求都被後面的檢查擋掉了,CORS 規則根本沒機會生效
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
